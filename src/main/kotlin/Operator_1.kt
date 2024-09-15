00f30c17df5330f59faf2f3ab87237faaf823d1d
ASUS-PC <rizkiafriani20@gmail.com>
2024-09-15 11:53:09 +0700

    add Operator_1.kt

---
+++ src/main/kotlin/Operator_1.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var myNumber: Int? = 15
+    myNumber = null
+    //println(myNumber *2)
+
+    var s1 = "Hello"
+    var s2: String? = "World"
+    println(s1.length)
+   // println(s2.length)
+}
